c276555b0e8b6183a23821795547a3ace077bc4c
samuelbslawrence <samuel.b.l.skelton@gmail.com>
2024-12-05 12:13:32 +0000

    Lab 2.2

---
+++ LabsWork/600098-Initial_ECS_Framework/Managers/ResourceManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using SkiaSharp;
+using OpenTK.Graphics.OpenGL;
+using OpenGL_Game.OBJLoader;
+
+namespace OpenGL_Game.Managers
+{
+    static class ResourceManager
+    {
+        static Dictionary<string, Geometry> geometryDictionary = new Dictionary<string, Geometry>();
+        static Dictionary<string, int> textureDictionary = new Dictionary<string, int>();
+
+        public static void RemoveAllAssets()
+        {
+            foreach(var geometry in geometryDictionary)
+            {
+                geometry.Value.RemoveGeometry();
+            }
+            geometryDictionary.Clear();
+            foreach(var texture in textureDictionary)
+            {
+                GL.DeleteTexture(texture.Value);
+            }
+            textureDictionary.Clear();
+        }
+
+        public static Geometry LoadGeometry(string filename)
+        {
+            Geometry geometry;
+            geometryDictionary.TryGetValue(filename, out geometry);
+            if (geometry == null)
+            {
+                geometry = new Geometry();
+                geometry.LoadObject(filename);
+                geometryDictionary.Add(filename, geometry);
+            }
+
+            return geometry;
+        }
+
+        public static int LoadTexture(string filename)
+        {
+            if (String.IsNullOrEmpty(filename))
+                throw new ArgumentException(filename);
+
+            int texture;
+            textureDictionary.TryGetValue(filename, out texture);
+            if (texture == 0)
+            {
+                texture = GL.GenTexture();
+                textureDictionary.Add(filename, texture);
+                GL.Enable(EnableCap.Texture2D);
+                GL.BindTexture(TextureTarget.Texture2D, texture);
+
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+                SKBitmap bmp = SKBitmap.Decode(filename);
+                if (bmp.ColorType == SKColorType.Bgra8888)
+                {
+                    GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, bmp.Width, bmp.Height, 0,
+                                  OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, bmp.GetPixels());
+                }
+
+                GL.BindTexture(TextureTarget.Texture2D, 0);
+                GL.Disable(EnableCap.Texture2D);
+            }
+
+            return texture;
+        }
+    }
+}
---
+++ LabsWork/600098-Initial_ECS_Framework/Systems/SystemRender.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using OpenTK.Graphics.OpenGL;
+using OpenGL_Game.Components;
+using OpenGL_Game.OBJLoader;
+using OpenGL_Game.Objects;
+using OpenGL_Game.Scenes;
+using OpenTK.Mathematics;
+
+namespace OpenGL_Game.Systems
+{
+    class SystemRender : ISystem
+    {
+        const ComponentTypes MASK = (ComponentTypes.COMPONENT_POSITION | ComponentTypes.COMPONENT_GEOMETRY);
+
+        protected int pgmID;
+        protected int vsID;
+        protected int fsID;
+        protected int uniform_stex;
+        protected int uniform_mmodelviewproj;
+        protected int uniform_mmodel;
+        protected int uniform_diffuse;
+
+        public SystemRender()
+        {
+            pgmID = GL.CreateProgram();
+            LoadShader("Shaders/single-light.vert", ShaderType.VertexShader, pgmID, out vsID);
+            LoadShader("Shaders/single-light.frag", ShaderType.FragmentShader, pgmID, out fsID);
+            GL.LinkProgram(pgmID);
+
+            GL.GetProgram(pgmID, GetProgramParameterName.LinkStatus, out int success);
+            if (success == 0)
+            {
+                string infoLog = GL.GetProgramInfoLog(pgmID);
+                Console.WriteLine(infoLog);
+            }
+
+            Console.WriteLine(GL.GetProgramInfoLog(pgmID));
+
+            uniform_stex = GL.GetUniformLocation(pgmID, "s_texture");
+            uniform_mmodelviewproj = GL.GetUniformLocation(pgmID, "ModelViewProjMat");
+            uniform_mmodel = GL.GetUniformLocation(pgmID, "ModelMat");
+            uniform_diffuse = GL.GetUniformLocation(pgmID, "v_diffuse");
+        }
+
+        void LoadShader(String filename, ShaderType type, int program, out int address)
+        {
+            address = GL.CreateShader(type);
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                GL.ShaderSource(address, sr.ReadToEnd());
+            }
+            GL.CompileShader(address);
+
+
+            GL.GetShader(address, ShaderParameter.CompileStatus, out int success);
+            if (success == 0)
+            {
+                string infoLog = GL.GetShaderInfoLog(address);
+                Console.WriteLine(infoLog);
+            }
+
+            GL.AttachShader(program, address);
+        }
+
+        public string Name
+        {
+            get { return "SystemRender"; }
+        }
+
+        public void OnAction(Entity entity)
+        {
+            if ((entity.Mask & MASK) == MASK)
+            {
+                List<IComponent> components = entity.Components;
+
+                IComponent geometryComponent = components.Find(delegate(IComponent component)
+                {
+                    return component.ComponentType == ComponentTypes.COMPONENT_GEOMETRY;
+                });
+                Geometry geometry = ((ComponentGeometry)geometryComponent).Geometry();
+
+                IComponent positionComponent = components.Find(delegate(IComponent component)
+                {
+                    return component.ComponentType == ComponentTypes.COMPONENT_POSITION;
+                });
+                Vector3 position = ((ComponentPosition)positionComponent).Position;
+                Matrix4 model = Matrix4.CreateTranslation(position);
+
+                Draw(model, geometry);
+            }
+        }
+
+        public void Draw(Matrix4 model, Geometry geometry)
+        {
+            GL.UseProgram(pgmID);
+
+            GL.Uniform1(uniform_stex, 0);
+            GL.ActiveTexture(TextureUnit.Texture0);
+
+            GL.UniformMatrix4(uniform_mmodel, false, ref model);
+            Matrix4 modelViewProjection = model * GameScene.gameInstance.camera.view * GameScene.gameInstance.camera.projection;
+            GL.UniformMatrix4(uniform_mmodelviewproj, false, ref modelViewProjection);
+
+            geometry.Render(uniform_diffuse);
+
+            GL.UseProgram(0);
+        }
+    }
+}
