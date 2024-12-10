9b710d84cd44f241110e9bcb2a00ae170ca71353
samuelbslawrence <samuel.b.l.skelton@gmail.com>
2024-12-10 13:17:03 +0000

    Networking Added
    Not yet functional

---
+++ LabsWork/600098-Client_Server/Client/Client.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Sockets;
+
+public class Client
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Starting echo client...");
+
+        // set port number
+        int port = 8080;
+        // create a client
+        TcpClient client = new TcpClient("localhost", port);
+        // get the stream
+        NetworkStream stream = client.GetStream();
+        // create a writer and reader
+        StreamReader reader = new StreamReader(stream);
+        StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+
+        try
+        {
+            while (true)
+            {
+                // read from the console
+                Console.Write("Enter text to send: ");
+                string lineToSend = Console.ReadLine();
+                // send to the server
+                Console.WriteLine("Sending to server: " + lineToSend);
+                writer.WriteLine(lineToSend);
+                // read from the server and print to the console
+                string lineReceived = reader.ReadLine();
+                Console.WriteLine("Received from server: " + lineReceived);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+
+        // close the connection
+        Console.WriteLine("Client disconnected from server.");
+        client.Close();
+    }
+}
---
+++ LabsWork/600098-Client_Server/Server/Server.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+public class Server
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Starting echo server...");
+
+        // set port number
+        int port = 8080;
+        // create a local listener
+        TcpListener listener = new TcpListener(IPAddress.Loopback, port);
+        // start listening
+        listener.Start();
+
+        // accept a client
+        TcpClient client = listener.AcceptTcpClient();
+        // get the stream
+        NetworkStream stream = client.GetStream();
+        // create a writer and reader
+        StreamWriter writer = new StreamWriter(stream, Encoding.ASCII) { AutoFlush = true };
+        StreamReader reader = new StreamReader(stream, Encoding.ASCII);
+
+        // read from the client and echo back
+        try
+        {
+            string inputLine = "";
+            while (inputLine != null)
+            {
+                // read from the client
+                inputLine = reader.ReadLine();
+                // echo back to the client
+                writer.WriteLine("Echoing string: " + inputLine);
+                // print to the console
+                Console.WriteLine("Echoing string: " + inputLine);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+
+        // close the connection
+        Console.WriteLine("Server disconnected from client.");
+        client.Close();
+        listener.Stop();
+    }
+}
